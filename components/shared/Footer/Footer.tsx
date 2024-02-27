5ac4c862defae9704af45b6fefcce7c775d17749
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-02-27 11:30:15 +0100

    Header and Footer done

--- components/shared/Footer/Footer.tsx
+++ components/shared/Footer/Footer.tsx
@@ -1,18 +1,76 @@
-// a
+//
 import Image from "next/image";
 import Link from "next/link";
 
-import { Logo } from "@/public";
+// @/.
+import { Logo, EmailFooter } from "@/public";
+import { Footer_QuickLinks, Footer_SocialLinks } from "@/constants/text";
+
+// ./
 import styles from "./styles.module.scss";
 
 const Footer = () => {
     return (
         <footer className={styles.footerContainer}>
             <div className={styles.footerContent}>
-                <Link href={"/"}>
-                    <Image src={Logo} alt="logo footer" />
-                </Link>
-                
+                <div className={styles.leftSide}>
+                    <Link href={"/"}>
+                        <Image src={Logo} alt="logo footer" />
+                    </Link>
+                    <p>
+                        Ratepunk compares hotel room prices across the major
+                        online travel agencies. When you search for a room,
+                        Ratepunk extension scans the top booking sites and runs
+                        a price comparison, so you can be confident in knowing
+                        you’re getting the best deal!
+                    </p>
+                </div>
+                <div className={styles.rightSide}>
+                    <div className={styles.rightSideContainer}>
+                        <h4 className={styles.rightSideTitle}>Quick links</h4>
+                        <ul className={styles.quickLinksList}>
+                            {Footer_QuickLinks.map((link) => (
+                                <li
+                                    key={link.id}
+                                    className={styles.quickLinkItem}
+                                >
+                                    <Link href={link.url}>{link.label}</Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className={styles.rightSideTwoContainers}>
+                        <div className={styles.rightSideContainer}>
+                            <h4 className={styles.rightSideTitle}>
+                                Quick links
+                            </h4>
+                            <Link
+                                href={"mailto:hi@ratepunk,com"}
+                                className={styles.linkEmail}
+                            >
+                                <Image src={EmailFooter} alt="icon" />
+                                <p>hi@ratepunk.com</p>
+                            </Link>
+                        </div>
+                        <div className={styles.rightSideContainer}>
+                            <h4 className={styles.rightSideTitle}>Social</h4>
+                            <div className={styles.socialLinksList}>
+                                {Footer_SocialLinks.map((link) => (
+                                    <Link
+                                        href={link.url}
+                                        className={styles.socilaLinkItem}
+                                        key={link.id}
+                                    >
+                                        <Image src={link.image} alt="sl" />
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p className={styles.copyright}>
+                    © 2021 Ratepunk. All Rights Reserved.
+                </p>
             </div>
         </footer>
     );
--- constants/text/index.ts
+++ constants/text/index.ts
@@ -1,20 +1,76 @@
 import { NavBar_Links_types } from "../types";
+import { Twitter, Facebook, TikTok, Instagram, LinkedIn } from "@/public";
 
 export const NavBar_Links: NavBar_Links_types[] = [
     {
         id: 1,
         label: "Chrome Extension",
-        url: 'chrome-extension'
+        url: "chrome-extension",
     },
     {
         id: 2,
         label: "Price Comparison",
-        url: 'price-comparison'
+        url: "price-comparison",
     },
     {
         id: 3,
         label: "Blog",
-        url: 'blog'
+        url: "blog",
     },
 ];
 
+export const Footer_QuickLinks = [
+    {
+        id: 1,
+        label: "Price Comparison",
+        url: "price-comparison",
+    },
+    {
+        id: 2,
+        label: "Chrome Extension",
+        url: "chrome-extension",
+    },
+    {
+        id: 3,
+        label: "How It Works",
+        url: "how-it-works",
+    },
+    {
+        id: 4,
+        label: "Ratepunk Blog",
+        url: "blog",
+    },
+    {
+        id: 5,
+        label: "Privacy Policy",
+        url: "privacy-policy",
+    },
+];
+
+export const Footer_SocialLinks = [
+    {
+        id: 1,
+        image: Instagram,
+        url: "",
+    },
+    {
+        id: 2,
+        image: Facebook,
+        url: "",
+    },
+    {
+        id: 3,
+        image: LinkedIn,
+        url: "",
+    },
+    {
+        id: 4,
+        image: Twitter,
+        url: "",
+    },
+    {
+        id: 5,
+        image: TikTok,
+        url: "",
+    },
+];
