a290ba070f86361ca7ba17db163cbe4d01b4d557
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-02-27 15:02:02 +0100

    FINAL COMMIT

--- components/shared/Hero/FormCopy/FormCopy.tsx
+++ components/shared/Hero/FormCopy/FormCopy.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
 
+//
+import Image from "next/image";
+import { useState } from "react";
+
+// @/
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Success } from "@/public";
+
+// ./
+import styles from "./styles.module.scss";
+
+// !
 const FormCopy = () => {
-  return (
-    <div>FormCopy</div>
-  )
-}
+    const [copied, setCopied] = useState(false);
+    const [urlValue, setUrlValue] = useState("https://ratepunk.com/referral");
 
-export default FormCopy
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(urlValue);
+        setCopied(true);
+        setTimeout(() => {
+            setCopied(false);
+        }, 2000);
+    };
+
+    return (
+        <div className={styles.formCopy}>
+            <Label className={styles.labelForm}>
+                <Image src={Success} alt="done" />
+                <p>Your email is confirmed!</p>
+            </Label>
+            <div className={styles.inputSector}>
+                <input type="text" value={urlValue} readOnly />
+                <Button onClick={copyToClipboard} variant={"bluebutton"}>
+                    {copied ? "Copied" : "Copy"}
+                </Button>
+            </div>
+        </div>
+    );
+};
+
+export default FormCopy;
